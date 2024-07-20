48dca8edf6718486c2571d1136894dfbfdd2cf23
nadavsc <nadavsch19ad@gmail.com>
2024-07-20 14:21:36 +0300

    update changes

---
+++ dataset.py
@@ -0,0 +1,115 @@
+import torch
+import glob
+import numpy as np
+import random
+import os
+
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+from datasets import load_from_disk, load_dataset
+
+from logger.logger import info
+
+
+class DataExtractor(Dataset):
+    def __init__(self, mode, lr_path, hr_path, transform=None):
+        self.mode = mode
+        self.lr_path = os.path.join(lr_path, 'arrow')
+        self.hr_path = os.path.join(hr_path, 'arrow')
+        self.transform = transform
+
+        self.lr_img, self.hr_img = self.load_datasets()
+
+    def save_to_disk(self, arrow_path):
+        os.makedirs(arrow_path)
+        db_path = os.path.dirname(arrow_path)
+        db_img = load_dataset(db_path, data_files={self.mode: '*.png'}, split=self.mode).with_format('torch')
+        db_img.save_to_disk(arrow_path)
+        return db_img
+
+    def load_datasets(self):
+        if os.path.exists(self.lr_path):
+            lr_img = load_from_disk(self.lr_path).with_format("torch")
+            info(f'{len(lr_img)} LR images have been loaded')
+        else:
+            lr_img = self.save_to_disk(self.lr_path)
+            info(f'{len(lr_img)} LR images have been loaded and saved to disk')
+
+        if os.path.exists(self.hr_path):
+            hr_img = load_from_disk(self.hr_path).with_format("torch")
+            info(f'{len(hr_img)} HR images have been loaded')
+        else:
+            hr_img = self.save_to_disk(self.hr_path)
+            info('{len(hr_img)} HR images have been loaded and saved to disk')
+        return lr_img, hr_img
+
+    def __len__(self):
+        return len(self.lr_img)
+
+    def __getitem__(self, i):
+        img_item = {}
+        lr_img = self.lr_img[i]
+        hr_img = self.hr_img[i]
+
+        img_item['lr'] = (lr_img / 127.5) - 1.0
+        img_item['hr'] = (hr_img / 127.5) - 1.0
+
+        if self.transform is not None:
+            img_item = self.transform(img_item)
+
+        img_item['lr'] = img_item['lr'].transpose(2, 0, 1)
+        img_item['hr'] = img_item['hr'].transpose(2, 0, 1)
+        return img_item
+
+
+class crop(object):
+    def __init__(self, scale, patch_size):
+        self.scale = scale
+        self.patch_size = patch_size
+
+    def __call__(self, sample):
+        lr_img, hr_img = sample['lr'], sample['hr']
+        ih, iw = lr_img.shape[:2]
+
+        ix = random.randrange(0, iw - self.patch_size + 1)
+        iy = random.randrange(0, ih - self.patch_size + 1)
+
+        tx = ix * self.scale
+        ty = iy * self.scale
+
+        lr_patch = lr_img[iy: iy + self.patch_size, ix: ix + self.patch_size]
+        hr_patch = hr_img[ty: ty + (self.scale * self.patch_size), tx: tx + (self.scale * self.patch_size)]
+
+        return {'lr': lr_patch, 'hr': hr_patch}
+
+
+class augmentation(object):
+    def __call__(self, sample):
+        lr_img, hr_img = sample['lr'], sample['hr']
+
+        hor_flip = random.randrange(0, 2)
+        ver_flip = random.randrange(0, 2)
+        rot = random.randrange(0, 2)
+
+        if hor_flip:
+            temp_lr = np.fliplr(lr_img)
+            lr_img = temp_lr.copy()
+            temp_hr = np.fliplr(hr_img)
+            hr_img = temp_hr.copy()
+
+            del temp_lr, temp_hr
+
+        if ver_flip:
+            temp_lr = np.flipud(lr_img)
+            lr_img = temp_lr.copy()
+            temp_hr = np.flipud(hr_img)
+            hr_img = temp_hr.copy()
+
+            del temp_lr, temp_hr
+
+        if rot:
+            lr_img = lr_img.transpose(1, 0, 2)
+            hr_img = hr_img.transpose(1, 0, 2)
+
+        return {'lr': lr_img, 'hr': hr_img}
+
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import os
 import argparse
+
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
+from dataset import DataExtractor, crop, augmentation
 from logger.logger import set_logger, info
 from preprocess import downscale_image
 from preprocess import load_dataset
@@ -7,20 +12,20 @@ from preprocess import load_dataset
 
 set_logger('./logger/log.txt')
 db_path = './dataset'
-db_train_hr_path = './dataset/DIV2K_train_HR'
-db_valid_hr_path = './dataset/DIV2K_valid_HR'
 downscale_factor = 4
 db_train_lr_path = f'./dataset/DIV2K_train_LRx{downscale_factor}'
 db_valid_lr_path = f'./dataset/DIV2K_valid_LRx{downscale_factor}'
+db_train_hr_path = './dataset/DIV2K_train_HR'
+db_valid_hr_path = './dataset/DIV2K_valid_HR'
 
 
-def downscale_images(hr_path, lr_path, downscale_factor):
+def downscale_images(lr_path, hr_path, downscale_factor):
     """
     Downscale HR images by a given factor using bicubic interpolation
     and saved in the given LR path.
 
-    :param hr_path: Path of the HR images directory.
     :param lr_path: Path of the LR images directory to be saved within.
+    :param hr_path: Path of the HR images directory.
     :param downscale_factor: Factor by which to downscale the image.
     """
     N = len(os.listdir(hr_path))
@@ -39,7 +44,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Examine several SR models and responsible on the pre-process.'
     )
-    parser.add_argument('--downscale', type=bool, default='False',
+    parser.add_argument("--scale", type=int, default=4, help='Scale for each patch in an image')
+    parser.add_argument("--patch_size", type=int, default=24, help='Number of patches for one image')
+    parser.add_argument('--batch_size', type=int, default=8, help='Size of the batch through train and test')
+    parser.add_argument('--downscale', type=bool, default=False,
                         help='Create a LR version of train and valid dataset')
     parser.add_argument('--downscale_factor', type=int, default='4', help='Specify the downscale factor')
     parser.add_argument('--device', type=str, default='cuda:0', help='Choose device cuda:0/cpu')
@@ -47,5 +55,17 @@ if __name__ == "__main__":
     args, unknown = parser.parse_known_args()
 
     if args.downscale:
-        downscale_images(hr_path=db_train_hr_path, lr_path=db_train_lr_path, downscale_factor=downscale_factor)
-        downscale_images(hr_path=db_valid_hr_path, lr_path=db_valid_lr_path, downscale_factor=downscale_factor)
+        downscale_images(lr_path=db_train_lr_path, hr_path=db_train_hr_path, downscale_factor=downscale_factor)
+        downscale_images(lr_path=db_valid_lr_path, hr_path=db_valid_hr_path, downscale_factor=downscale_factor)
+
+    transform = transforms.Compose([crop(args.scale, args.patch_size), augmentation()])
+    train_dataset = DataExtractor(mode='train',
+                                  lr_path=db_train_lr_path,
+                                  hr_path=db_train_hr_path,
+                                  transform=transform)
+    validation_dataset = DataExtractor(mode='validation',
+                                       lr_path=db_valid_lr_path,
+                                       hr_path=db_valid_hr_path,
+                                       transform=transform)
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, num_workers=args.num_workers)
+    validation_loader = DataLoader(validation_dataset, batch_size=args.batch_size, num_workers=args.num_workers)
